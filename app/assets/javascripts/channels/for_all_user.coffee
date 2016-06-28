2aff1d3160fdebc7248c3dda4badffdb95e8d052
Kremenchuk <kremenchuk@bk.ru>
2016-06-28 22:18:12 +0300

    1

--- app/assets/javascripts/channels/for_all_user.coffee
+++ app/assets/javascripts/channels/for_all_user.coffee
@@ -1,15 +1,19 @@
-App.chat = App.cable.subscriptions.create {
-  channel: "ForAllUserChannel"
-},
-  connected: ->
-# Called when the subscription is ready for use on the server
+$(document).ready ->
+  App.chat = App.cable.subscriptions.create {
+    channel: "ForAllUserChannel"
+  },
+    connected: ->
+  # Called when the subscription is ready for use on the server
 
-  disconnected: ->
-# Called when the subscription has been terminated by the server
+    disconnected: ->
+  # Called when the subscription has been terminated by the server
 
-  received: (data) ->
-    alert data['message']
+    received: (data) ->
+      if data['type'] == 'message'
+        $('#message_field').append data['message']
+      else
+        $('#user_field').append data['message']
 
-  send_message: (message) ->
-    @perform 'send_message', message: message
+    send_message: (message) ->
+      @perform 'send_message', message: message
 
--- app/channels/for_all_user_channel.rb
+++ app/channels/for_all_user_channel.rb
@@ -1,15 +1,19 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class ForAllUserChannel < ApplicationCable::Channel
   def subscribed
-     stream_from "for_all_user_#{current_user.id}_channel"
+    stream_from "for_user_#{current_user.id}_channel"
+    stream_from "for_all_user_channel"
+    a=ApplicationController.render partial: 'welcome/user', locals: {user:current_user}
+    ActionCable.server.broadcast "for_all_user_channel", message: a
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+#    a=ApplicationController.render partial: 'welcome/user', locals: {user:current_user}
+#    ActionCable.server.broadcast "for_all_user_#{current_user.id}_channel", message: a
   end
 
   def send_message(data)
-    a=data['message']
-    ActionCable.server.broadcast "for_all_user_#{current_user.id}_channel", message: data['message']
+    a= ApplicationController.render partial: 'welcome/message', locals: {message: data['message']}
+    ActionCable.server.broadcast "for_user_#{current_user.id}_channel", message: a,type: 'message'
   end
 end
